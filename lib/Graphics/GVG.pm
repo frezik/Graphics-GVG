236a0eead3efe62faebb52ebd6301098e177bd6c
tmurray <tmurray@wumpus-cave.net>
2016-11-28 17:10:14 -0600

    Add documentation

--- lib/Graphics/GVG.pm
+++ lib/Graphics/GVG.pm
@@ -399,10 +399,169 @@ __END__
 
 =head1 SYNOPSIS
 
+    my $SCRIPT = <<'END';
+        %color = #FF33FFFF;
+
+        line( %color, 0.0, 0.0, 1.0, 1.1 );
+        glow {
+            circle( %color, 0, 0, 0.9 );
+            rect( %color, 0, 1, 0.7, 0.4 );
+        }
+    END
+
+    my $gvg = Graphics::GVG->new;
+    my $ast = $gvg->parse( $SCRIPT );
+
 =head1 DESCRIPTION
+
+Parses scripts that describe vectors used for gaming graphics. The script is 
+parsed into an Abstract Syntax Tree (AST), which can then be transformed into 
+different forms. For example, the OpenGL renderer generates Perl code that 
+can be compiled and called inside a larger Perl/OpenGL program.
+
+=head1 LANGUAGE
+
+Compared to SVG, GVG scripts are very simple. They look like a series of 
+C function calls, with blocks that generate various effects. Each statement 
+is ended by a semicolon.
+
+The coordinate space follows general OpenGL conventions, where x/y coords 
+are floating point numbers between -1.0 and 1.0, using the right-hand rule.
+
+=head2 Comments
+
+Comments start with '//' and go to the end of the line
+
+=head2 Operators
+
+There aren't any.
+
+=head2 Conditionals
+
+There aren't any.
+
+=head2 Loops
+
+There aren't any. I said this was a simple language, remember?
+
+=head2 Data Types
+
+GVG functions can take several data types:
+
+=over
+
+=item * Integer -- a series of digits with no decimal point, like C<1234>.
+
+=item * Float -- a series of digits, which can contain a decimal point, like C<1.234>. While you can specify as many digits as you want, note that these are ultimately limited to double-precision IEEE floats.
+
+=item * Color -- starts with a '#', and then is followed by 8 hexidecimal digits, in RGBA form, like C<#5cd2bbff>. Hex digits can be upper or lower case.
+
+=back
+
+Integers and floats can both use '-' to indicate a negative number.
+
+=head2 Variables
+
+Data types can be saved in variables, which each data type getting its own 
+sigal.
+
+    &x = 2; // Integer
+    $y = 1.23; // Float
+    %color = #ff33aaff; // Color
+
+    poly( %color, 0, $y, 4.3, &x, 30.2 );
+
+Variables can be redefined at any time:
+
+    %color = #ff33aaff;
+    line( %color, 0, 1, 1, 0 );
+    %color = #aabbaaff;
+    line( %color, 1, 0, 1, 1 );
+
+=head2 Functions
+
+There are several drawing functions for defining vectors.
+
+=head3 line
+
+  line( %color, $x1, $y1, $x2, $y2 );
+
+A line of the given C<%color>, going from coordinates C<$x1,$y1> to C<$x2,$y2>.
+
+=head3 circle
+
+  circle( %color, $cx, $cy, $r );
+
+A circle of the given C<%color>, centered at C<$cx,$cy>, with radius C<$r>.
+
+=head3 rect
+
+  rect( %color, $x, $y, $width, $height );
+
+A rectangle of the given C<%color>, starting at C<$x,$y>, and then going to 
+C<$x + $width> and C<$y + $height>.
+
+=head3 ellipse
+
+  ellipse( %color, $cx, $cy, $rx, $ry );
+
+An ellipse of the given C<%color>, centered at C<$cx,$cy>, with respective radii
+C<$rx> and C<$ry>.
+
+=head3 poly
+
+  poly( %color, $cx, $cy, &sides, $r );
+
+A regular polygon of the given C<%color>, centered at C<$cx,$cy>, with radius 
+C<$r>, and C<&sides> number of sides.
+
+=head2 Effects
+
+Effects can be applied to drawing functions by enclosing them in a block 
+(inside C<{...}> characters) named for a certain effect.
+
+For example, a glow effect can be set on lines with:
+
+    glow {
+        circle( %color, 0, 0, 0.9 );
+        rect( %color, 0, 1, 0.7, 0.4 );
+    }
+
+How this is rendered is dependent on the renderer.  An OpenGL renderer may 
+show an actual neon glow effect, while a renderer for a physics library 
+may ignore it entirely.
 
 =head1 METHODS
 
 =head2 parse
+
+Takes a GVG script as input. On success, returns an abstract syntax tree.  
+Otherwise, throws a fatal error.
+
+=head1 LICENSE
+
+    Copyright (c) 2016  Timm Murray
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without 
+    modification, are permitted provided that the following conditions are met:
+
+        * Redistributions of source code must retain the above copyright notice, 
+          this list of conditions and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright 
+          notice, this list of conditions and the following disclaimer in the 
+          documentation and/or other materials provided with the distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+    POSSIBILITY OF SUCH DAMAGE.
 
 =cut
